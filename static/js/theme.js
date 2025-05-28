1cb00e7d73f2475e6ae97b97d6572dc1f1f43454
genfuru011 <hiro_genfuru0119@icloud.com>
2025-05-28 21:57:00 +0900

    /staticにjsディレクトリ作成

---
+++ static/js/theme.js
@@ -0,0 +1,28 @@
+const themeToggleButton = document.getElementById('theme-toggle-button');
+const htmlElement = document.documentElement;
+
+        // localStorageからテーマを読み込む
+        const savedTheme = localStorage.getItem('pico-theme') || 'light';
+        htmlElement.setAttribute('data-theme', savedTheme);
+        updateButtonText();
+
+        themeToggleButton.addEventListener('click', () => {
+            let currentTheme = htmlElement.getAttribute('data-theme');
+            if (currentTheme === 'light') {
+                htmlElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('pico-theme', 'dark');
+            } else {
+                htmlElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('pico-theme', 'light');
+            }
+            updateButtonText();
+        });
+
+        function updateButtonText() {
+            const currentTheme = htmlElement.getAttribute('data-theme');
+            if (currentTheme === 'light') {
+                themeToggleButton.textContent = 'dark mode';
+            } else {
+                themeToggleButton.textContent = 'light mode';
+            }
+        }
